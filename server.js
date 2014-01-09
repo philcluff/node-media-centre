33892124f4898a77715d8b89c23f43daf8c0d42d
Phil Cluff <philcluff@googlemail.com>
2014-01-09 00:06:58 +0000

    It works!

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
+// Load in modules
 var restify = require('restify');
 var url = require('url');
 var exec = require('child_process').exec,
     child;
 
 // Configuration loaded from ENV
-var player = process.env.OMX_PLAYER
+var player = process.env.OMX_PLAYER;
 
+// Global state
 var requests = 0;
 var playing = 0;
 
 // Start the Media player given a file URL
 var startPlayer = function(fileUrl) {
 
-    var command = player + ' -b ' + fileUrl;
-    var child = exec(command, function(err, stdout, stderr) {
+    var command = player + ' ' + fileUrl;
+    console.log(command);
+    child = exec(command, function(err, stdout, stderr) {
 	    if (err) throw err;
 	    else {
 		// When the child process returns, we can mark the player as stopped.
@@ -34,7 +37,7 @@ function play(req, res, next) {
     console.log('Request ID: ' + requests);
 
     if (playing === 0) {
-	startPlayer();
+	startPlayer(req.body.url);
 	res.send(201, "Playback Started.");
     }
     else {
@@ -43,10 +46,47 @@ function play(req, res, next) {
     }
 }
 
+// Stop API call
+function stop(req, res, next) {
+
+    requests++;
+    console.log('Got request to stop!');
+    console.log('Request ID: ' + requests);
+
+    if (playing === 1) {
+	child.stdin.write('q');
+	res.send(200, "Playback Stopped.");
+    }
+    else {
+	console.log("Not playing");
+	res.send(200, "Not playing anything.");
+    }
+}
+
+// Stop API call
+function pause(req, res, next) {
+
+    requests++;
+    console.log('Got request to pause!');
+    console.log('Request ID: ' + requests);
+
+    if (playing === 1) {
+	child.stdin.write('p');
+	res.send(200, "Playback paused/unpaused.");
+    }
+    else {
+	console.log("Not playing");
+	res.send(200, "Not playing anything.");
+    }
+}
+
+// Startup REST server and setup request mappings
 var server = restify.createServer();
 
 server.use(restify.bodyParser());
 server.post('/api/player/play', play);
+server.post('/api/player/stop', stop);
+server.post('/api/player/pause', pause);
 
 server.listen(8080, function() {
     console.log('%s listening at %s', server.name, server.url);
