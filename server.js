4a45884f7cae2d6b63b23227a66e7bbb63e74516
Phil Cluff <philcluff@googlemail.com>
2014-01-11 00:39:43 +0000

    Startings of a library api

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 var restify = require('restify');
 var mediaPlayer = require("media-player");
+var mediaLibrary = require("plex");
 
 // Startup REST server and setup request mappings
 var server = restify.createServer();
 
+// Media Player API
 server.use(restify.bodyParser());
 server.post('/api/player/play', mediaPlayer.play);
 server.post('/api/player/stop', mediaPlayer.stop);
 server.post('/api/player/pause', mediaPlayer.pause);
+
+// Media Library API
+server.get('/api/media/tvshows', plex.getAllTvShows);
 
 server.listen(8080, function() {
     console.log('%s listening at %s', server.name, server.url);
