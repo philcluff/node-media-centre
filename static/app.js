d5635d0a7f892c105b90fef31e2aec9c13d6eb09
Phil Cluff <philcluff@googlemail.com>
2014-01-15 00:45:28 +0000

    Working walking menu. Stub play behaviour.

--- static/app.js
+++ static/app.js
@@ -1,17 +1,57 @@
-function loadRoot() {
+function loading() {
     $( "#loading" ).text("Loading...");
+}
+function doneLoading() {
+    $( "#loading" ).text("");
+}
+
+function loadRoot() {
+    loading();
     var jqxhr = $.getJSON( "/api/media/tvshows", function(data) {
         var items = [];
         for (var i = 0; i < data.length; i++) {
-            items.push("<li onclick=\"loadSeriesList(this.id)\" id=\"" + data[i].attributes.key  + "\">" + data[i].attributes.title + "</li>");
+            items.push("<li onclick=\"loadSeriesList(this.id)\" id=\"" + data[i].attributes.ratingKey  + "\">" + data[i].attributes.title + "</li>");
         }
         $( "#show" ).append(items.join( "" ));
-        $( "#loading" ).text("");
+	doneLoading();
     });
 }
 
 function loadSeriesList(key) {
+    loading();
+    $( "#series" ).empty();
+    $( "#episode" ).empty();
     console.log("Click: " + key);
+    var jqxhr = $.getJSON( "/api/media/children/" + key, function(data) {
+        var items = [];
+        for (var i = 0; i < data.length; i++) {
+	    if (data[i].attributes.ratingKey) {
+		items.push("<li onclick=\"loadEpisodeList(this.id)\" id=\"" + data[i].attributes.ratingKey  + "\">" + data[i].attributes.title + "</li>");
+	    }
+        }
+        $( "#series" ).append(items.join( "" ));
+	doneLoading();
+    });
+}
+
+function play() {
+    alert("Play!");
+}
+
+function loadEpisodeList(key) {
+    loading();
+    $( "#episode" ).empty();
+    console.log("Click: " + key);
+    var jqxhr = $.getJSON( "/api/media/children/" + key, function(data) {
+        var items = [];
+        for (var i = 0; i < data.length; i++) {
+	    if (data[i].attributes.ratingKey) {
+		items.push("<li onclick=\"play()\" id=\"" + data[i].attributes.ratingKey  + "\">" + data[i].attributes.title + "</li>");
+	    }
+        }
+        $( "#episode" ).append(items.join( "" ));
+	doneLoading();
+    });
 }
 
 function keyListener() {
