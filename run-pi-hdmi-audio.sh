324e9161b5d99d2aa7105dc8e93c4317a87256db
Phil Cluff <philcluff@googlemail.com>
2014-04-01 23:43:39 +0100

    Fixed other scripts to set Plex port

--- run-pi-hdmi-audio.sh
+++ run-pi-hdmi-audio.sh
@@ -1,3 +1,4 @@
 export PLEX_SERVER='yoshi'
 export OMX_PLAYER='omxplayer '
+export PLEX_SERVER_PORT='32400'
 node server.js
--- run-pi-local-audio.sh
+++ run-pi-local-audio.sh
@@ -1,3 +1,4 @@
 export OMX_PLAYER='omxplayer -o local '
 export PLEX_SERVER='yoshi'
+export PLEX_SERVER_PORT='32400'
 node server.js
